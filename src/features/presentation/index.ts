71fab229d4992b3797c5213233f0f8e1ad151b2d
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2026-09-25 11:52:53 -0300

    feat(presentation): add feature index exports

---
+++ src/features/presentation/index.ts
@@ -0,0 +1,5 @@
+export { PresentationOverlay } from './components/presentation-overlay';
+export { usePresentation } from './hooks/use-presentation';
+export { resetPresentation } from './state/presentation-state';
+export { ACTS, getAct } from './constants/acts';
+export type { Act, ActStep } from './constants/acts';
